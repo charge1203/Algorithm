046a59e7d868c98a18e9ff03988db96b28787c11
Jiyeon Cha <102739179+charge1203@users.noreply.github.com>
2023-08-28 18:42:29 +0900

    Add files via upload

---
+++ programmers/introduction/문자열계산하기.py
@@ -0,0 +1,14 @@
+def solution(my_string):
+    my_string = my_string.split()
+    answer = int(my_string[0])
+    for i in range(1, len(my_string), 2):
+        if my_string[i] == '+':
+            answer += int(my_string[i + 1])
+        elif my_string[i] == '-':
+            answer -= int(my_string[i + 1])
+
+    return answer
+
+# 다른풀이
+def solution2(my_string):
+    return eval(my_string)
