79eb634f032023573d0f9007b60f1e9e4b788b17
Jiyeon Cha <102739179+charge1203@users.noreply.github.com>
2023-11-24 01:14:48 +0900

    Add files via upload

---
+++ programmers/줄서는방법.py
@@ -0,0 +1,16 @@
+# 다시 이해하고 풀어보기
+
+import math
+
+
+def solution(n, k):
+    arr = [i for i in range(1, n + 1)]
+    answer = []
+
+    while arr:
+        a = (k - 1) // math.factorial(n - 1)
+        answer.append(arr.pop(a))
+        k = k % math.factorial(n - 1)
+        n -= 1
+
+    return answer
