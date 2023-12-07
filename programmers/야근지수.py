690e9f2440ef3535e9123496d4307bd4872eed96
Jiyeon Cha <102739179+charge1203@users.noreply.github.com>
2023-12-07 10:50:09 +0900

    Add files via upload

---
+++ programmers/야근지수.py
@@ -0,0 +1,18 @@
+# 문제가 잘 이해 안됨
+import heapq
+
+def solution(n, works):
+    if sum(works) <= n:
+        return 0
+
+    answer = 0
+    works = [-w for w in works]
+    heapq.heapify(works)
+    while n > 0:
+        max_val = heapq.heappop(works)
+        heapq.heappush(works, max_val + 1)
+        n -= 1
+
+    for w in works:
+        answer += w ** 2
+    return answer
